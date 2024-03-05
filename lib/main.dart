242f5a044a6646baef6db200526401f5707327ac
FLLAN <florent@lanternier.net>
2024-03-05 12:28:57 +0100

    App initialization optimized

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,45 @@ import 'package:octattoo_app/home/home.dart';
 import 'package:octattoo_app/loading.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:octattoo_app/providers/locale_manager_provider.dart';
-import 'package:octattoo_app/services/locale_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() async {
+void main() {
+  runApp(const ProviderScope(child: AppInitializer()));
+}
+
+class AppInitializer extends StatelessWidget {
+  const AppInitializer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const MaterialApp(
+              home: Scaffold(
+                  body: Center(child: Text('Error initializing app'))));
+        }
+        // Show a loading screen while waiting for initialization to complete
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const MaterialApp(home: LoadingScreen());
+        }
+        // Once complete, return the actual app
+        return const App();
+      },
+    );
+  }
+}
+
+Future<void> initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await LocaleManager().initializeLocale();
-  runApp(
-    const ProviderScope(
-      child: App(),
-    ),
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  // Include any other initializations here.
 }
 
 class App extends ConsumerStatefulWidget {
@@ -26,47 +52,15 @@ class App extends ConsumerStatefulWidget {
 }
 
 class _AppState extends ConsumerState<App> {
-  late final Future<FirebaseApp> _initialization;
-
-  @override
-  void initState() {
-    super.initState();
-    _initialization = Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final localeManager = ref.watch(localeManagerProvider);
-    
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('Error initializing Firebase'),
-              ),
-            ),
-          );
-        }
 
-        // Show a loading screen while waiting for initialization to complete
-        if (snapshot.connectionState != ConnectionState.done) {
-          return const MaterialApp(home: LoadingScreen());
-        }
-
-        // Once complete, show your application
-        return MaterialApp(
-          home: const HomeScreen(),
-          locale: localeManager.locale,
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: AppLocalizations.supportedLocales,
-        );
-      },
+    return MaterialApp(
+      home: const HomeScreen(),
+      locale: localeManager.locale,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
