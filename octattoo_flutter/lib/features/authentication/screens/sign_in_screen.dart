7872352151a8727e952a2d640dcab5e0ae266350
Florent LANTERNIER <inscription@lanternier.net>
2025-04-03 09:54:12 +0200

    feat: :sparkles: let's implement a router

---
+++ octattoo_flutter/lib/features/authentication/screens/sign_in_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:octattoo_flutter/features/authentication/components/sign_in_form.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SignInForm();
+  }
+}
